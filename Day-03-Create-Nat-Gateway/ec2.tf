e0c80200c40ebb76da1796dfb9cca1b13e007a79
ramya <ramyaakula457@gmail.com>
2024-07-17 16:31:18 +0530

    file day-3 updated

---
+++ Day-03-Create-Nat-Gateway/ec2.tf
@@ -0,0 +1,23 @@
+
+resource "aws_instance" "Pavan" {
+    ami = var.ami_id_Public
+    instance_type = var.instance_type_01
+    key_name = var.key_name_01
+    subnet_id = aws_subnet.public_ramya.id
+    associate_public_ip_address = true #Enable auto-assigning public IP
+    tags = {
+      Name = "Public-Instance-01"
+    }
+  
+}
+resource "aws_instance" "Private_ramya" {
+    ami = var.ami_id_Private
+    instance_type = var.instance_type_02
+    key_name = var.key_name_02
+    subnet_id = aws_subnet.Private_ramya.id
+    associate_public_ip_address = false #Disable auto-assigning Public IP
+    tags = {
+      Name = "Priavte-Instance-01"
+    }
+  
+}

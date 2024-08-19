96d5dd2568bb830c00fcd5615e36b12719768492
ramya <ramyaakula457@gmail.com>
2024-08-19 16:17:14 +0530

    updates

--- Day-01-BasiCode/main.tf
+++ Day-01-BasiCode/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Gayathri" {
     ami = "ami-0a4408457f9a03be3"
     instance_type = "t2.micro"
-    key_name = "jenkins"
+    key_name = "Jenkins"
     tags = {
       Name = "instance-01"
     }
